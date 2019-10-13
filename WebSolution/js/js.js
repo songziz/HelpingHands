d5c9181120097a64e264c916326013167e4ac11d
Zizhen Song <songziz@uw.edu>
2019-10-12 21:57:23 -0700

    Added tabs and working map

--- WebSolution/js/js.js
+++ WebSolution/js/js.js
@@ -6,3 +6,22 @@ $(function(){
     var map = L.map('map').fitBounds(bounds);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 })
+
+function closeModal() {
+    document.getElementById("menu").style.display = "none";
+    viewer.destroy();
+}
+
+function tabOver(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
